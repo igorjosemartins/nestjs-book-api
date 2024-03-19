6f0afbe36e6507d89d234dd2d683c863fddf2165
Igor Martins <igor.martins@ibridge.com.br>
2024-03-19 12:16:29 -0300

    sign up logic : update user + bookmarks models | generate password hash with argon2 + save the user in db | catch duplicate field prisma error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
-export class AuthService{
-    constructor(private prisma: PrismaService) {}
-    
-    signup(dto: AuthDto) {
-        return { msg: 'I have signed up ' }
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    async signup(dto: AuthDto) {
+
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        try {
+            // save the new user in the db
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash
+                }
+            });
+
+            delete user.hash;
+
+            // return the saved user
+            return user;
+
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // duplicate field prisma error
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
+            }
+
+            throw error;
+        }
     }
 
     signin() {
