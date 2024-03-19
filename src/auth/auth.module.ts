9badc282e918f649ff7e727cabf6798840569490
Igor Martins <igor.martins@ibridge.com.br>
2024-03-19 11:52:14 -0300

    creating auth dtos with nest class-tranformer and class-validator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { AuthController } from "./auth.controler";
+import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
